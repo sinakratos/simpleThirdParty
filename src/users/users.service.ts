e0b8aa8cb606e008a410409769f8d7222bf9e80c
sinakratos <sinakhoshzaban2@gmail.com>
2025-12-09 18:29:18 +0330

    Added Users and import in app.madule

---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schema/user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async findOrCreate(data: any) {
+    let user = await this.userModel.findOne({ email: data.email });
+
+    if (!user) {
+      user = new this.userModel(data);
+      await user.save();
+    }
+
+    return user;
+  }
+}
